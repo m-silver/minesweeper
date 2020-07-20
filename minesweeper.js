58a0308fb5ccfeed237305e4d8fb4e1017af9e77
Matt Silver <mattsilver@windowslive.com>
2020-07-20 14:39:59 +1200

    Add surrounding mine count

--- minesweeper.js
+++ minesweeper.js
@@ -4,12 +4,20 @@ var board = {
   cells: [
     {row: 0, col: 0, isMine: true, isHidden: true, hidden: true}, 
     {row: 0, col: 1, isMine: false, isHidden: true, hidden: true}, 
-    {row: 1, col: 0, isMine: false, isHidden: true, hidden: true}, 
-    {row: 1, col: 1, isMine: false, isHidden: true, hidden: true}
+    {row: 0, col: 2, isMine: false, isHidden: true, hidden: true}, 
+    {row: 1, col: 0, isMine: false, isHidden: true, hidden: true},
+    {row: 1, col: 1, isMine: true, isHidden: true, hidden: true}, 
+    {row: 1, col: 2, isMine: false, isHidden: true, hidden: true}, 
+    {row: 2, col: 0, isMine: false, isHidden: true, hidden: true}, 
+    {row: 2, col: 1, isMine: false, isHidden: true, hidden: true},
+    {row: 2, col: 2, isMine: false, isHidden: true, hidden: true}
   ]
 }
 
 function startGame () {
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+  }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -25,14 +33,14 @@ function checkForWin () {
   // if the property isMine = true, also check if isMarked = true
   // return winning message
   
-  let result = board.cells.every((cell) => (cell.isMine == false && cell.hidden == false) || (cell.isMine == true && cell.isMarked == true));
+  /*let result = board.cells.every((cell) => (cell.isMine == false && cell.hidden == false) || (cell.isMine == true && cell.isMarked == true));
 
   console.log(result)
 
   if (result == true) {
     lib.displayMessage('You win!')
   }
-  
+  */
 
 
 
@@ -54,5 +62,14 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col);
+  var count = 0;
+
+  for (var i = 0; i < surrounding.length; i++) {
+    if (surrounding[i].isMine == true) {
+      count++;
+    }
+  }
+  return count
 }
 
