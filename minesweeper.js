f674d1110dd6a470fd34c7c5614eb262dc1bf64f
Matt Silver <mattsilver@windowslive.com>
2020-07-20 21:02:50 +1200

    Add board generator with random mines

--- minesweeper.js
+++ minesweeper.js
@@ -1,23 +1,48 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-
-var board = {
-  cells: [
-    {row: 0, col: 0, isMine: true, isHidden: true, hidden: true}, 
-    {row: 0, col: 1, isMine: false, isHidden: true, hidden: true}, 
-    {row: 0, col: 2, isMine: false, isHidden: true, hidden: true}, 
-    {row: 1, col: 0, isMine: false, isHidden: true, hidden: true},
-    {row: 1, col: 1, isMine: true, isHidden: true, hidden: true}, 
-    {row: 1, col: 2, isMine: false, isHidden: true, hidden: true}, 
-    {row: 2, col: 0, isMine: false, isHidden: true, hidden: true}, 
-    {row: 2, col: 1, isMine: false, isHidden: true, hidden: true},
-    {row: 2, col: 2, isMine: false, isHidden: true, hidden: true}
+var board = { }
+  /*cells: [
+    {row: 0, col: 0, isMine: (Math.random() < 0.5), isMarked: true, hidden: true}, 
+    {row: 0, col: 1, isMine: (Math.random() < 0.5), isMarked: true, hidden: true}, 
+    {row: 0, col: 2, isMine: (Math.random() < 0.5), isMarked: true, hidden: true}, 
+    {row: 1, col: 0, isMine: (Math.random() < 0.5), isMarked: true, hidden: true},
+    {row: 1, col: 1, isMine: (Math.random() < 0.5), isMarked: true, hidden: true}, 
+    {row: 1, col: 2, isMine: (Math.random() < 0.5), isMarked: true, hidden: true}, 
+    {row: 2, col: 0, isMine: (Math.random() < 0.5), isMarked: true, hidden: true}, 
+    {row: 2, col: 1, isMine: (Math.random() < 0.5), isMarked: true, hidden: true},
+    {row: 2, col: 2, isMine: (Math.random() < 0.5), isMarked: true, hidden: true}
   ]
+}*/
+
+function createBoard () {
+  // assign property cells which has a value of an array to board object
+  // add an object to the cells array, with properties of row, col, ismine, isMarked and hidden
+  // do this x (4,9,16) times
+  board.cells = []
+
+  for (var i = 0; i < 3; i++) {
+    board.cells.push({row: 0, col: (i), isMine: (Math.random() < 0.5), isMarked: false, hidden: true})
+    } 
+  for (var i = 0; i < 3; i++) {
+    board.cells.push({row: 1, col: (i), isMine: (Math.random() < 0.5), isMarked: false, hidden: true})
+    } 
+  for (var i = 0; i < 3; i++) {
+    board.cells.push({row: 2, col: (i), isMine: (Math.random() < 0.5), isMarked: false, hidden: true})
+    } 
+
 }
+
+
+
+
+
+
 
 function startGame () {
   document.addEventListener('click', checkForWin);
   document.addEventListener('contextmenu', checkForWin);
+
+  createBoard()
 
   for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
@@ -31,28 +56,11 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-
-  // iterate through each object in the array
-  // if the property isMine = false, also check if hidden = false
-  // if the property isMine = true, also check if isMarked = true
-  // return winning message
   
   if (board.cells.every((cell) => (cell.isMine == false && cell.hidden == false) || (cell.isMine == true && cell.isMarked == true))) {
-    lib.displayMessage('You win!')
+    lib.displayMessage('You win!');
     }
   }
-
-
-
-
-
-
-
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
-
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
@@ -62,6 +70,7 @@ function checkForWin () {
 //
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
+
 function countSurroundingMines (cell) {
   var surrounding = lib.getSurroundingCells(cell.row, cell.col);
   var count = 0;
